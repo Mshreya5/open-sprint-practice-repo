069a173eb41784246342fcc637978ec610e927af
varsha-kotegar <varshakotegar26@gmail.com>
2026-03-05 05:54:40 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function showQuote() {
+    document.getElementById("quote").innerText =
+    "Believe in yourself!";
+}
+const quotes = [
+"Believe in yourself",
+"Never give up",
+"Dream big",
+"Stay focused"
+];
